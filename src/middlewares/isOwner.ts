f7a131809404f7ebff6c95104f7dedbf96a01155
Bachir Seghir <bachir@BMacM.local>
2023-07-17 23:31:39 +0100

    added new middleware isOwner

---
+++ src/middlewares/isOwner.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response, NextFunction } from 'express'
+
+
+const isOwner = async (req: Request, res: Response, next: NextFunction) => {
+
+    const userId = req.params.id;
+    console.log();
+
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+export default isOwner
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,13 +1,14 @@
 import express from 'express'
 import { register, login, getAllUsers, getUserById } from '../controllers/userController'
 import authenticate from '../middlewares/authenticate'
+import isOwner from '../middlewares/isOwner'
 
 const router = express.Router()
 
 router.post('/register', register)
 router.post('/login', login)
 router.get('/', authenticate, getAllUsers)
-router.get('/:id', getUserById)
+router.get('/:id', isOwner, getUserById)
 
 
 export default router
